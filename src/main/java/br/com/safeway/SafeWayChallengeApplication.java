9c6b7ef08763dbab556becf866500ae55a52d9a2
Nyrvlivy <nyrvlivy@gmail.com>
2024-03-24 21:11:31 -0300

    chore: initial project setup

---
+++ src/main/java/br/com/safeway/SafeWayChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.safeway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeWayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeWayChallengeApplication.class, args);
+	}
+
+}
